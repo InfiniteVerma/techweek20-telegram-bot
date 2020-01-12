5b5483ed9a3125ab5bc3afa23b05a639fba65848
InfiniteVerma <vermaanant25@gmail.com>
2020-01-12 21:06:12 +0530

    fix bug: printing 'thanks for submitting multiple times

--- bot.js
+++ bot.js
@@ -36,70 +36,112 @@ const requestPhoneKeyboard = {
     ]
   }
 };
-
+var submittedPhoneNumber = false;
 //register command to store user information
 bot.onText(/\/register/, msg => {
-  bot.sendMessage(
-    msg.chat.id,
-    "Got it! Starting your form, " +
-      msg.chat.first_name +
-      " " +
-      msg.chat.last_name
-  );
-  bot
-    .sendMessage(msg.chat.id, "Enter email", {
-      reply_markup: JSON.stringify({ force_reply: true })
-    })
-    .then(sentMessage => {
-      bot.onReplyToMessage(
-        sentMessage.chat.id,
-        sentMessage.message_id,
-        reply => {
-          if (reply.text == "") {
-            console.log("error");
-          } else {
-            email = reply.text;
-            console.log(reply.text);
-            bot
-              .sendMessage(msg.chat.id, "Enter yes if outstation", {
-                reply_markup: JSON.stringify({ force_reply: true })
-              })
-              .then(sentMessage => {
-                bot.onReplyToMessage(
-                  sentMessage.chat.id,
-                  sentMessage.message_id,
-                  reply => {
-                    if (reply.text == "") {
-                      console.log("error");
-                    } else {
+  if (phone_number != undefined) {
+    // bot.sendMessage(msg.chat.id, "You've already entered your details");
+    bot.sendMessage(
+      msg.chat.id,
+      "Got it! Editing your details, " + msg.chat.first_name
+    );
+    bot
+      .sendMessage(msg.chat.id, "Enter email", {
+        reply_markup: JSON.stringify({ force_reply: true })
+      })
+      .then(sentMessage => {
+        bot.onReplyToMessage(
+          sentMessage.chat.id,
+          sentMessage.message_id,
+          reply => {
+            if (reply.text == "") {
+              console.log("error");
+            } else {
+              email = reply.text;
+              console.log(reply.text);
+              bot
+                .sendMessage(msg.chat.id, "Enter yes if outstation", {
+                  reply_markup: JSON.stringify({ force_reply: true })
+                })
+                .then(sentMessage => {
+                  bot.onReplyToMessage(
+                    sentMessage.chat.id,
+                    sentMessage.message_id,
+                    reply => {
                       outstation = reply.text;
                       console.log(reply.text);
-                      bot
-                        .sendMessage(
-                          msg.chat.id,
-                          "Can we get access to your phone number?",
-                          requestPhoneKeyboard
-                        )
-                        .then(sentMessage => {
-                          sentMessage.chat.id,
-                            sentMessage.message_id,
-                            bot.on("contact", function(msg, match) {
-                              console.log(msg.contact.phone_number);
-                              phone_number = msg.contact.phone_number;
-                              bot.sendMessage(
-                                msg.chat.id,
-                                "Thanks for entering the form. Check out /details"
-                              );
-                            });
-                        });
+                      bot.sendMessage(
+                        msg.chat.id,
+                        "Form successfully edited. Check out your new details at /details"
+                      );
                     }
-                  }
-                );
-              });
+                  );
+                });
+            }
           }
-        }
-      );
-    });
+        );
+      });
+  } else {
+    bot.sendMessage(
+      msg.chat.id,
+      "Got it! Starting your form, " + msg.chat.first_name
+    );
+    bot
+      .sendMessage(msg.chat.id, "Enter email", {
+        reply_markup: JSON.stringify({ force_reply: true })
+      })
+      .then(sentMessage => {
+        bot.onReplyToMessage(
+          sentMessage.chat.id,
+          sentMessage.message_id,
+          reply => {
+            if (reply.text == "") {
+              console.log("error");
+            } else {
+              email = reply.text;
+              console.log(reply.text);
+              bot
+                .sendMessage(msg.chat.id, "Enter yes if outstation", {
+                  reply_markup: JSON.stringify({ force_reply: true })
+                })
+                .then(sentMessage => {
+                  bot.onReplyToMessage(
+                    sentMessage.chat.id,
+                    sentMessage.message_id,
+                    reply => {
+                      if (reply.text == "") {
+                        console.log("error");
+                      } else {
+                        outstation = reply.text;
+                        console.log(reply.text);
+
+                        bot
+                          .sendMessage(
+                            msg.chat.id,
+                            "Can we get access to your phone number?",
+                            requestPhoneKeyboard
+                          )
+                          .then(sentMessage => {
+                            sentMessage.chat.id,
+                              sentMessage.message_id,
+                              bot.on("contact", function(msg, match) {
+                                bot.sendMessage(
+                                  msg.chat.id,
+                                  "Thanks for entering your details! Check out /details"
+                                );
+                                console.log(msg.contact.phone_number);
+                                phone_number = msg.contact.phone_number;
+                              });
+                          });
+                      }
+                    }
+                  );
+                });
+            }
+          }
+        );
+      });
+  }
 });
 
 //details command shows user information stored
@@ -113,8 +155,6 @@ bot.onText(/\/details/, msg => {
         msg.chat.id,
         "Name: " +
           msg.chat.first_name +
-          " " +
-          msg.chat.last_name +
           "\nEmail: " +
           email +
           "\nOutstation: " +
@@ -185,7 +225,6 @@ bot.onText(/\/pp/, msg => {
 bot.onText(/\/hack/, msg => {
   bot.sendMessage(msg.chat.id, "Hackathon Event Descriptions...");
 });
-
 
 var eventOptions = {
   reply_markup: {
