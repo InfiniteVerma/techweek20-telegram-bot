1f561573081e7cbe4482abba7a45a410669a09f7
InfiniteVerma <vermaanant25@gmail.com>
2020-01-17 09:52:25 +0530

    ads

--- start/developers.js
+++ start/developers.js
@@ -2,7 +2,5 @@ var bot = require("../bot");
 // start command
 bot.onText(/\/developers/, msg => {
   console.log(msg.chat.id);
-  bot.sendMessage(msg.chat.id, 'Developer Credits: \n'+
-  '1. Anant Verma\n' + 
-  '2. Shubham Sachdeva')
+  bot.sendMessage(msg.chat.id, 'Developers: \nKishor (mentor)\nAnant\nShubham')
 });
