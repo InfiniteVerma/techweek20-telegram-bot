67ca9c1ea9be06b888c1b5e942a6770324c37120
InfiniteVerma <vermaanant25@gmail.com>
2020-01-18 00:00:01 +0530

    asdf

--- eventDetails/hack.js
+++ eventDetails/hack.js
@@ -3,5 +3,8 @@ var bot = require("../bot");
 bot.onText(/\/hackDetails/, msg => {
   bot.sendMessage(msg.chat.id, "Hackathon Event:\n\n"+
   "Hackathon – A 20 hour Hackathon based on the theme of ‘safety, security and surveillance’. Innovate, build and validate your idea in one go.\n"+
-  "\nTo Register: /hackathon");
+  "To view all problem statements among other details, view the brochure given below\n"+
+  "\nTo Register: /hackathon").then(()=>{
+    bot.sendDocument(msg.chat.id, './iamges/hack4cause.pdf')
+  })
 });
