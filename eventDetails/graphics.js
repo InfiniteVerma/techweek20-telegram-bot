2f85e67216f363a322c51d0135af3683803c54c2
InfiniteVerma <vermaanant25@gmail.com>
2020-01-17 10:04:00 +0530

    a

--- eventDetails/graphics.js
+++ eventDetails/graphics.js
@@ -2,7 +2,7 @@
 var bot = require("../bot");
 //Describe the hackathon
 bot.onText(/\/graphics/, msg => {
-  bot.sendMessage(msg.chat.id, "Hackathon Event Description:\n\n"+
+  bot.sendMessage(msg.chat.id, "Graphics Event:\n\n"+
   "Magna Odyssey – Depict your adventure through unknown lands using a limited set of tools. Emerge the victor and take away your design on a T-shirt.\n"+
   "\nTo Register: /MagnaOdeyssey");
 });
